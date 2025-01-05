ad16e872814835bd0184ad30bbae0f34567f11f7
Danish Muhammed <130174434+dani-zed@users.noreply.github.com>
2025-01-05 16:02:05 +0530

    user model and  authentication added

--- api/src/routes/auth.route.js
+++ api/src/routes/auth.route.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { updateProfile } from "../controllers/auth.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
+import { checkAuth } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
 router.post("/logout", logout);
+
+router.put("/update-profile",auth,updateProfile);
+router.get("/check",auth,checkAuth)
 
 export default router;
